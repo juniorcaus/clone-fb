b11b07dfcd0d725b4ff9278b16c6ba99110661a6
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-05-09 21:34:23 -0300

    Criando AuthProvider

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -3,8 +3,41 @@ import { createContext, useEffect, useState } from 'react';
 export const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] =useState(null);
+
+    useEffect(() => {
+        function loadStorage() {
+            const storageUser = localStorage.getItem("AuthFaceClone");
+
+            if (storageUser) {
+                setUser(JSON.parse(storageUser));
+            }
+        }
+
+        loadStorage();
+    }, []);
+
+    const signInWithFacebook = (res) => {
+        let data = {
+            name: res.name,
+            email: res.email,
+            avatarUrl: res.picture.data.url,
+        };
+
+        setUser(data);
+        localStorage.setItem("AuthFaceClone", JSON.stringify(data));
+    };
+
+    function signOut() {
+        localStorage.removeItem("AuthFaceClone");
+
+        setUser(null);
+    }
+
     return (
         <AuthContext.Provider>
+            value={{ signInWithFacebook, signOut, user }}
+
             {children}
         </AuthContext.Provider>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import AuthProvider from './contexts/auth';
 
 const root = createRoot(document.querySelector("#root"));
 
-root.render(<App />);
+root.render(
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+);
