25a1f0fd3ed97cd0e98455f3d050b4d446f27ba6
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-05-10 21:38:32 -0300

    Criando componente Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Login from './components/Login';
 import GlobalStyles from './styles/global';
+import { AuthContext } from './contexts/auth';
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) return <Login />;
+
   return (
     <>
-      TESTE 1111111111111111111111111111111
+      TESTE 1111111111111111111111111111
       <GlobalStyles />
     </>
   );
---
+++ src/components/Login/index.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { FacebookLogin } from "react-facebook-login";
+import { SiFacebook } from "react-icons/si";
+import { AuthContext } from "../../contexts/auth";
+import GlobalStyles from "../../styles/global";
+import * as C from "./styles";
+
+const Login = () => {
+    const { signInWithFacebook } = useContext(AuthContext);
+
+    return (
+        <>
+            <C.Container>
+                <SiFacebook />
+                <FacebookLogin appId="4473557302745218" fields="name, email, picture" callback={signInWithFacebook} />
+            </C.Container>
+            <GlobalStyles />
+        </>
+
+    );
+};
+
+export default Login;
